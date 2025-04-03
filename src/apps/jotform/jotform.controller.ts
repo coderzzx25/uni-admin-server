cc089cbafba4b8c986fadf18dcfcc13f41607c4c
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-04-03 09:29:27 +0800

    Add -> webhook -> 添加测试

--- src/apps/jotform/jotform.controller.ts
+++ src/apps/jotform/jotform.controller.ts
@@ -3,7 +3,7 @@ import { Body, Controller, Headers, Post, Req } from '@nestjs/common';
 @Controller('jotform')
 export class JotformController {
   @Post('webhook')
-  async jotFormWebhook(@Body() body: any) {
-    console.log('jotFormWebhook', body);
+  async jotFormWebhook(@Req() req: any) {
+    console.log('jotFormWebhook', req);
   }
 }
