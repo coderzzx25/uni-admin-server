e52db107f67f48cbbc7d4273cbc90b4de7ebb50d
coderzzx <coderzzx@coderzzxMacBook-Air.local>
2025-04-03 09:49:00 +0800

    Add -> webhook -> 添加测试

--- src/apps/jotform/jotform.module.ts
+++ src/apps/jotform/jotform.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JotformController } from './jotform.controller';
 import { JotformService } from './jotform.service';
+import { NestjsFormDataModule } from 'nestjs-form-data';
 
 @Module({
+  imports: [NestjsFormDataModule],
   controllers: [JotformController],
   providers: [JotformService],
 })
