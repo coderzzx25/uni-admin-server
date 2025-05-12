4a1cf4163ef0ab2f7eb81083d830f091e33100c4
coderzzx <2549518530@qq.com>
2025-05-12 10:41:17 +0800

    Mod -> test -> 添加测试

--- src/apps/test/test.controller.ts
+++ src/apps/test/test.controller.ts
@@ -1,5 +1,16 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
 
+interface IInquiryData {
+  No_Label_name: string;
+  No_Label_email: string;
+  No_Label_message: string;
+  No_Label_field_c70cdcb: string;
+  No_Label_field_b127923: string;
+  No_Label_field_18f8b6d: string;
+  No_Label_field_e063889: string;
+  form_id: string;
+}
+
 @Controller('test')
 export class TestController {
   /**
@@ -8,5 +19,6 @@ export class TestController {
   @Post('inquiry')
   inquiry(@Body() body: any, @Req() req: any) {
     console.log(body, req);
+    return 'ok';
   }
 }
